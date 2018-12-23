2d2aa6a55f58939a0493a0dab5d77e052848034f
CryTime <zoulou.snmarn@laposte.net>
2018-12-23 22:12:03 +0100

    add connexion and registration

--- connexion.php
+++ connexion.php
@@ -1,100 +1,20 @@
 <!DOCTYPE html>
-
-<!DOCTYPE html>
 <html>
 <head>
 	<title>Site de Rencontre</title>
 </head>
 <body>
-	<script>
-
-			/*******************************************************************
-			 *@method : Fonction qui regarde si tous nos champs sont correctes.*
-			 *@param  : Prends les champs du formulaire en paramètre.          *   
-			 *@return : Renvoie un booléen	                                   *  
-			 *******************************************************************/	
-
-			function CheckInput(f){
-				var pseudoOk   = CheckBasicInput(f.pseudo);
-				var passwordOk = CheckBasicInput(f.pass);
-				var emailOk    = CheckEmail(f.email); 
-
-				if(pseudoOk && passwordOk && emailOk){
-					return true;
-				}else{
-					alert("Erreur");
-					return false;
-				}
-
-			}
-
-			/*********************************************************************
-			 *@method : Fonction qui regarde si un champ a une longueur correcte.* 
-			 *@param  : Prend en parametre le champ                              *
-			 *@return : Renvoie un booléen                                       *
-			 *********************************************************************/	
-
-			function CheckBasicInput(champ){
-				if(champ.value.length < 2 || champ.value.length > 25){
-					Highlight(champ, true);
-					return false;
-				}else{
-					Highlight(champ, false);
-					return true;
-				}
-			}
-
-			/***************************************************************************************
-			 *@method : Fonction qui test si on viens de rentrer une email avec un format correcte.*
-			 *@param  : Prend en parametre le champ                                                *
-			 *@return : Renvoie un booléen                                                         *
-			 ***************************************************************************************/
-
-			function CheckEmail(champ){
-				var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
-
-				console.log(regex.test(champ.value));
-
-				if(!regex.test(champ.value)){
-					Highlight(champ, true);
-					return false;
-				}else{
-					Highlight(champ, false);
-					return true;
-				}
-			}
-
-			/***************************************************************************************
-			 *@method : Fonction qui surligne le background du champ en rouge si il y a une erreur.*
-			 *@param  : Prend en paramètre le champ et le booléen true or false.                   *
-			 *@return : Renvoie un void                                                            *
-			 ***************************************************************************************/
-			
-			function Highlight(champ, erreur){
-				if(erreur){
-					champ.style.backgroundColor = "#fba";
-				}else{
-					champ.style.backgroundColor = "";
-				}
-			}
-		</script>
-
-	<form method="post" action="save_in_bdd.php" onsubmit="return CheckInput(this)">
+	<form method="post" action="read_in_bdd.php">
 		<p>
-			<label for="pseudo">Votre Pseudo : </label>
-			<input type="text" name="pseudo" id="pseudo" onblur="CheckBasicInput(this)"/>
+			<label for="pseudo"> Votre Pseudo : </label>
+			<input type="text" name="pseudo">
 			</br>
-			<label for="pass">Votre mot de passe : </label>
-			<input type="password" name="pass" id="pass" onblur="CheckBasicInput(this)"/>
-			</br>
-			<label for="email">Votre email : </label>
-			<input type="text" name="email" id="email" onblur="CheckEmail(this)"/>
+			<label for="pass"> Votre mot de passe : </label>
+			<input type="password" name="pass">
 			</br>
 			<input type="submit" value="Valider"/>
 		</p>
-	</form>
 
-	<a href="read_in_bdd.php"> id enregistrer </a>
-	
+	</form>
 </body>
 </html>
--- read_in_bdd.php
+++ read_in_bdd.php
@@ -1,13 +1,30 @@
 <!DOCTYPE HTML>
 <html>
 <?php 
-	 
-	$bdd = new PDO('mysql:host=localhost;dbname=SiteDeRecontre;charset=utf8', 'root', '');
-		
-	$answer = $bdd->query('SELECT * FROM User');
+	
+	$pseudo   = NULL;
+	$password = NULL;
 
+	if(isset($_POST['pseudo']) && empty($_POST['pseudo']) == false &&
+	   isset($_POST['pass'])   && empty($_POST['pass'])   == false){
+		$pseudo   = htmlspecialchars($_POST['pseudo']);
+		$password = hash('sha256', htmlspecialchars($_POST['pass']));
+
+	}else{
+		echo "<script type=\"text/javascript\">alert(\"Champs incorrectes\"); location=\"connexion.php\"</script>";
+			exit(1);
+	} 
+	
+	$bdd    = new PDO('mysql:host=localhost;dbname=SiteDeRecontre;charset=utf8', 'root', '');
+	$answer = $bdd->query('SELECT pseudo, password FROM User WHERE pseudo="'.$pseudo.'"');
+	
 	while($datadb = $answer->fetch()){
-		echo '<p>' . htmlspecialchars($datadb['pseudo']) . ' ' . htmlspecialchars($datadb['password']) . '</p>';
+		if($datadb['password'] == $password){
+			echo "Ok :)";
+		}else{
+			echo "<script type=\"text/javascript\">alert(\"Champs incorrectes\"); location=\"connexion.php\"</script>";
+			exit(1);
+		}
 	}
 
 	$answer->closeCursor();
