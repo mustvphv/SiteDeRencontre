bfb6dcffe02bd7262a054c2ef5b4312c9c14d517
CryTime <zoulou.snmarn@laposte.net>
2018-12-23 18:59:58 +0100

    more comments

--- connexion.php
+++ connexion.php
@@ -7,6 +7,13 @@
 </head>
 <body>
 	<script>
+
+			/*******************************************************************
+			 *@method : Fonction qui regarde si tous nos champs sont correctes.*
+			 *@param  : Prends les champs du formulaire en paramètre.          *   
+			 *@return : Renvoie un booléen	                                   *  
+			 *******************************************************************/	
+
 			function CheckInput(f){
 				var pseudoOk   = CheckBasicInput(f.pseudo);
 				var passwordOk = CheckBasicInput(f.pass);
@@ -21,6 +28,12 @@
 
 			}
 
+			/*********************************************************************
+			 *@method : Fonction qui regarde si un champ a une longueur correcte.* 
+			 *@param  : Prend en parametre le champ                              *
+			 *@return : Renvoie un booléen                                       *
+			 *********************************************************************/	
+
 			function CheckBasicInput(champ){
 				if(champ.value.length < 2 || champ.value.length > 25){
 					Highlight(champ, true);
@@ -30,6 +43,12 @@
 					return true;
 				}
 			}
+
+			/***************************************************************************************
+			 *@method : Fonction qui test si on viens de rentrer une email avec un format correcte.*
+			 *@param  : Prend en parametre le champ                                                *
+			 *@return : Renvoie un booléen                                                         *
+			 ***************************************************************************************/
 
 			function CheckEmail(champ){
 				var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
@@ -45,6 +64,12 @@
 				}
 			}
 
+			/***************************************************************************************
+			 *@method : Fonction qui surligne le background du champ en rouge si il y a une erreur.*
+			 *@param  : Prend en paramètre le champ et le booléen true or false.                   *
+			 *@return : Renvoie un void 	                                                       *
+        	 ***************************************************************************************/
+			
 			function Highlight(champ, erreur){
 				if(erreur){
 					champ.style.backgroundColor = "#fba";
