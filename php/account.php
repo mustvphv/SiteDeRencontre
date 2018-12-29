1d22bbce28fa985cbbef546f846ef33a53b13918
CryTime <zoulou.snmarn@laposte.net>
2018-12-29 15:32:57 +0100

    security issue

--- php/account.php
+++ php/account.php
@@ -7,22 +7,60 @@
     <body>
         <?php
             function DisplayProfile(){
+            	
+
             	session_start();
             	$id = htmlspecialchars($_SESSION['id']);
-        
+       			
+       			
+
          		$bdd    = new PDO('mysql:host=localhost;dbname=SiteDeRecontre;charset=utf8', 'root', '');
 				$answer = $bdd->query('SELECT * FROM User WHERE id="'.$id.'"');
 				$datadb = $answer->fetch();
+		
+				$pseudoclient = $datadb['pseudo'];
+
+				$reponseUser         = $bdd->query('SELECT * FROM User WHERE pseudo!="'.$pseudoclient.'"');
+				$reponseLocalisation = $bdd->query('SELECT * FROM Localisation WHERE pseudo!="'.$pseudoclient.'"'); 
+				$reponsePhysique     = $bdd->query('SELECT * FROM Physique WHERE pseudo!="'.$pseudoclient.'"');
+				while($valueUser = $reponseUser->fetch()){
+					$valueLocalisation = $reponseLocalisation->fetch();		
+					$valuePhysique     = $reponsePhysique->fetch();  
 		?>		
+
+				<script  type="text/javascript"> 
+					var clientsMap = new Map();
+					clientsMap.set(<?php echo json_encode($valueUser['pseudo']); ?>              , 
+						         [<?php echo json_encode($valueUser['email']);  ?>              , 
+						          <?php echo json_encode($valueLocalisation['departement']); ?> ,
+						          <?php echo json_encode($valueLocalisation['region']); ?>      ,
+						          <?php echo json_encode($valuePhysique['clientgenre']); ?>     ,
+						          <?php echo json_encode($valuePhysique['clientpoids']); ?>	    ,
+						          <?php echo json_encode($valuePhysique['clientcouleurspeaux']); ?>
+						          ]);
+					
+				
+					console.log(clientMap)
+
+				</script>
+
+		
+		<?php		
+				}
+		?>		
+	
+		
 		<script type="text/javascript">
-			//conversion php to js 
+			
 			var pseudo = <?php echo json_encode($datadb['pseudo']); ?>;
 			var email  = <?php echo json_encode($datadb['email']);  ?>;
 			//console.log(pseudo, email); (Ok)
 			document.write('<p>' + pseudo + ' ' + email + '</p>');
 
-		</script>	
+		</script>
+
 		
+
 
 		<?php		
 				//echo "Ton pseudo : " .$datadb['pseudo']. "\n";
